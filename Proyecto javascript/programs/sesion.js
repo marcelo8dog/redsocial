e90296930debada0fb41d06f3e7488a667e45bd2
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2017-05-21 01:59:25 -0500

    Add files via upload

--- Proyecto javascript/programs/sesion.js
+++ Proyecto javascript/programs/sesion.js
@@ -32,15 +32,14 @@ $("#formu").on("submit",function(){
             $('.dropdown-button').dropdown({
               inDuration: 300,
               outDuration: 225,
-              constrainWidth: false, // Does not change width of dropdown to that of the activator
-              hover: true, // Activate on hover
-              gutter: 0, // Spacing from edge
-              belowOrigin: false, // Displays dropdown below the button
-              alignment: 'left', // Displays dropdown with edge aligned to the left of button
-              stopPropagation: false // Stops event propagation
-            }
-          );
-        },2000);
+              constrainWidth: false,
+              hover: true,
+              gutter: 0,
+              belowOrigin: false,
+              alignment: 'left',
+              stopPropagation: false
+            });
+        },7000);
       }
     }
   });
