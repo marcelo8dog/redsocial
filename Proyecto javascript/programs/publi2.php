7690447044e121d9d568acfab026e775904ba8c2
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2017-05-21 02:49:05 -0500

    Add files via upload

--- Proyecto javascript/programs/publi2.php
+++ Proyecto javascript/programs/publi2.php
@@ -28,30 +28,34 @@
     <li>
       <div onclick='collapsible()' class='collapsible-header'><i class='material-icons'>textsms</i>Comentarios</div>";
       	$pub=$fila['id_publicacion'];
-      		if($pub>0)
+        if($pub>0)
       		{
-      			$quecom="SELECT comentario.texto_comentario,comentario.tiempo_comentario FROM comentario JOIN publicacion ON publicacion.id_publicacion=comentario.id_publicacion WHERE comentario.id_publicacion='".$pub."'";
+      			$quecom="SELECT comentario.texto_comentario,comentario.tiempo_comentario,comentario.id_usuario FROM comentario JOIN publicacion ON publicacion.id_publicacion=comentario.id_publicacion WHERE comentario.id_publicacion='".$pub."'";
       		}
 			$imporcom=mysqli_query($cone,$quecom);
 			$filacom=mysqli_fetch_assoc($imporcom);
 			$rellcom="";
 			while($filacom)
 			{
-				$rellcom=$rellcom."<div class='collapsible-body'><span>".$filacom['texto_comentario']."<p>".$filacom['tiempo_comentario']."</p></span></div>";
+				$idus=$filacom['id_usuario'];
+				$quecomu="SELECT usuario.nombre_usuario FROM publicacion JOIN usuario ON usuario.id_usuario=publicacion.id_usuario WHERE usuario.id_usuario='".$idus."'";
+
+				$imporcomu=mysqli_query($cone,$quecomu);
+				$filacomu=mysqli_fetch_assoc($imporcomu);
+
+				$rellcom=$rellcom."<div class='collapsible-body'><h1>".$filacomu['nombre_usuario'].":"."</h1><span>".$filacom['texto_comentario']."<p>".$filacom['tiempo_comentario']."</p></span></div>";
         		$filacom=mysqli_fetch_assoc($imporcom);
 			}
 			$rell=$rell.$rellcom."
     </li>
   </ul>
               	<div class='row'>
-      <form method='POST' action='adentro2.php'>
         <div class='row'>
           <div class='input-field col s6'>
-            <input id='input_text' name='com".$fila['id_publicacion']."' type='text' data-length='30'>
+            <input id='input_text' type='text' data-length='30' onblur='sendi(".$fila['id_publicacion'].")'>
             <label for='input_text'>Comentar</label>
           </div>
         </div>
-      </form>
         </div>
             </div>
         </div>";
